a0a4a53d6b13c8f30d3220a784defb1a344ca309
mohammed eldeeb <mohammedeldeeb550@gmail.com>
2026-02-25 00:54:32 +0200

    Adminorder_added approved -rejected

--- ECommerce.Presentation.WinForms/Forms/Login/LoginForm.cs
+++ ECommerce.Presentation.WinForms/Forms/Login/LoginForm.cs
@@ -1,11 +1,12 @@
-﻿using ECommerce.ApplicationLayer.DTOs.LoginDtos;
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ECommerce.ApplicationLayer.DTOs.LoginDtos;
 using ECommerce.ApplicationLayer.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Web.WebView2.WinForms;
-using System;
-using System.IO;
-using System.Text.Json;
-using System.Windows.Forms;
 
 namespace ECommerce.Presentation.WinForms.Forms
 {
@@ -30,45 +31,77 @@ namespace ECommerce.Presentation.WinForms.Forms
 
         private async void InitializeWebView()
         {
-            webView = new WebView2
-            {
-                Dock = DockStyle.Fill
-            };
-
+            webView = new WebView2 { Dock = DockStyle.Fill };
             this.Controls.Add(webView);
 
+            // تأكد من WebView2 Runtime مثبت
             await webView.EnsureCoreWebView2Async();
 
             string pagePath = Path.Combine(Application.StartupPath, "UI", "login.html");
+
+            // مؤقتًا للتأكد من المسار
+            if (!File.Exists(pagePath))
+            {
+                MessageBox.Show($"Login HTML not found at: {pagePath}");
+                return;
+            }
+
             webView.Source = new Uri(pagePath);
 
             webView.CoreWebView2.WebMessageReceived += async (s, e) =>
             {
                 var root = JsonDocument.Parse(e.WebMessageAsJson).RootElement;
+                string action = root.GetProperty("action").GetString();
 
-                if (root.GetProperty("action").GetString() == "login")
+                if (action == "login")
                 {
-                    string email = root.GetProperty("email").GetString();
-                    string password = root.GetProperty("password").GetString();
-
-                    var dto = new LoginDto
-                    {
-                        Email = email,
-                        Password = password
-                    };
-
-                    var user = _userService.Login(dto);
-                    if (user != null)
-                    {
-                        var main = Program.ServiceProvider.GetRequiredService<MainForm>();
-                        main.SetUser(user.Id);
-
-                        this.Hide();
-                        await Task.Delay(500);
-                        main.ShowDialog();
-                    }
+                    await HandleLogin(root);
+                }
+                else if (action == "goToRegister")
+                {
+                    await OpenRegisterForm();
                 }
             };
+        }
+
+        private async Task HandleLogin(JsonElement root)
+        {
+            string email = root.GetProperty("email").GetString();
+            string password = root.GetProperty("password").GetString();
+
+            var dto = new LoginDto { Email = email, Password = password };
+            var user = _userService.Login(dto);
+
+            if (user != null)
+            {
+                Form targetForm;
+
+                // ⚡ تحديد الفورم حسب الدور
+                if (user.Role == Domain.Enums.UserRole.Admin)
+                    targetForm = Program.ServiceProvider.GetRequiredService<AdminDashboardForm>();
+                else
+                    targetForm = Program.ServiceProvider.GetRequiredService<MainForm>();
+
+                if (targetForm is MainForm mainForm)
+                    mainForm.SetUser(user.Id);
+
+                this.Hide();
+                await Task.Delay(300);
+                targetForm.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid email or password!");
+            }
+        }
+
+        private async Task OpenRegisterForm()
+        {
+            var register = Program.ServiceProvider.GetRequiredService<RegisterForm>();
+            this.Hide();
+            register.ShowDialog();
+            this.Show();
         }
     }
 }
